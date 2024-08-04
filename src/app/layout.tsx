acbd302340dba665ca1cadb6efc987428dfe0bc4
matine <mghazanfari@tarasystem.ch>
2024-08-04 21:55:10 +0330

    [feat]- get blogs api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientWrapper from "@/app/clientWrapper";
+import ReduxProvider from "@/app/reduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,9 +26,11 @@ export default function RootLayout({
     </head>
 
       <body className={inter.className}>
-        <ClientWrapper>
-          {children}
-        </ClientWrapper>
+        <ReduxProvider>
+          <ClientWrapper>
+            {children}
+          </ClientWrapper>
+        </ReduxProvider>
       </body>
     </html>
   );
