cee33d1ec957c5001de9cc3f9730072e0b176db4
matine <mghazanfari@tarasystem.ch>
2024-08-04 21:56:57 +0330

    [feat]- use api

--- src/app/components/Blogs/index.tsx
+++ src/app/components/Blogs/index.tsx
@@ -1,5 +1,34 @@
+'use client'
+import {Button} from "@/app/components/common/Button";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getBlogsLoading} from "@/app/store/getBlogs/action";
+import {getBlogDetailLoading} from "@/app/store/getBlogDetail/action";
 export default function Blogs() {
+    const dispatch = useDispatch();
+    const blogsStates = useSelector((state: any) => state.getBlogs)
+    const selectedBlog = useSelector((state: any) => state.getBlogDetail)
+
+    useEffect(() => {
+        // dispatch(getBlogsLoading())
+        dispatch(getBlogDetailLoading(3))
+    }, []);
+
+    // useEffect(() => {
+    //     console.log('states', blogsStates)
+    // }, [blogsStates]);
+
+
+    useEffect(() => {
+        console.log('selectedBlog', selectedBlog)
+    }, [selectedBlog]);
+
+
+
     return (
-        <div>Blogs</div>
+        <div>
+            Blogs
+            {/*<Button>my button</Button>*/}
+        </div>
     )
 }
