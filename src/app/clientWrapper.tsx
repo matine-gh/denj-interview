0676ac774bf1cdf190a5ae2ee9191d5181806187
matine <mghazanfari@tarasystem.ch>
2024-08-05 11:44:15 +0330

    [fix]- move themeProvider wrapper to clientWrapper.tsx

--- src/app/clientWrapper.tsx
+++ src/app/clientWrapper.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, {ReactNode, useEffect, useState} from "react";
 import Header from "@/app/components/common/Header";
+import {myTheme} from "@/app/theme";
+import {ThemeProvider} from "react-jss";
 
 export default function ClientWrapper({children}: { children: ReactNode }) {
     const [load, setLoad] = useState<boolean>(false);
@@ -8,12 +10,12 @@ export default function ClientWrapper({children}: { children: ReactNode }) {
         setLoad(true)
     }, [])
     return (
-        <>{
+        <ThemeProvider theme={myTheme}>{
             load &&
             <div>
                 <Header/>
                 {children}
             </div>
-        }</>
+        }</ThemeProvider>
     )
 }
